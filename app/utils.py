bf29926fa98f100057b818238f13d10590f76a92
narayanakvl <narayanakvl1122@gmail.com>
2025-01-26 21:14:14 +0530

    Initial version 0.1v added timestamp to downloading file

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 from faker import Faker
+from datetime import datetime
 
 fake = Faker()
 
@@ -21,8 +22,10 @@ def generate_fake_data(rows, columns):
     # Create the directory if it doesn't exist
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
+
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     
     # Save the file
-    file_path = os.path.join(output_dir, 'fake_data.xlsx')  # Full path to the file
+    file_path = os.path.join(output_dir, f'fake_data_{timestamp}.xlsx')  # Full path to the file
     df.to_excel(file_path, index=False)
     return file_path
